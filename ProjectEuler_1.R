474a16aba2e5410447be43c2cd1ccf35baa488ca
jeremypmobley <jeremypmobley@gmail.com>
2014-09-13 09:10:19 -0500

    Problem1

---
+++ ProjectEuler_1.R
@@ -0,0 +1,27 @@
+
+
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+
+resultguy <- data.frame()
+i <- 1
+while (i<1000){
+  if(i %% 3==0 | i %% 5 ==0 ){
+    
+    resultguy <- rbind(resultguy, i)
+  }
+  i <- i +1
+}
+
+sum(resultguy)
+
+
+
+
+
+
+
